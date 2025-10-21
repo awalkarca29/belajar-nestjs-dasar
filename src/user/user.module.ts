2903a5a3cf0b01ff6a1db7b253bb4dc25002b805
Awwalul <awwalul.cgs18@gmail.com>
2025-10-21 17:38:03 +0700

    Initial Commit Until Shared Module

---
+++ src/user/user.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { Connection, createConnection } from './connection/connection';
+import { mailService, MailService } from './mail/mail.service';
+import {
+  createUserRepository,
+  UserRepository,
+} from './user-repository/user-repository';
+import { MemberService } from './member/member.service';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  controllers: [UserController],
+  providers: [
+    UserService,
+    {
+      provide: Connection,
+      useFactory: createConnection,
+      inject: [ConfigService],
+    },
+    {
+      provide: MailService,
+      useValue: mailService,
+    },
+    {
+      provide: 'EmailService',
+      useExisting: MailService,
+    },
+    {
+      provide: UserRepository,
+      useFactory: createUserRepository,
+      inject: [Connection],
+    },
+    MemberService,
+  ],
+  exports: [UserService],
+})
+export class UserModule {}
---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,97 @@
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Inject,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+
+import type { HttpRedirectResponse } from '@nestjs/common';
+import type { Response, Request } from 'express';
+import { UserService } from './user.service';
+import { Connection } from '../connection/connection';
+import { MailService } from '../mail/mail.service';
+import { UserRepository } from '../user-repository/user-repository';
+import { MemberService } from '../member/member.service';
+
+@Controller('/api/users')
+export class UserController {
+  constructor(
+    private service: UserService,
+    private connection: Connection,
+    private mailService: MailService,
+    @Inject('EmailService') private emailService: MailService,
+    private userRepository: UserRepository,
+    private memberService: MemberService,
+  ) {}
+
+  @Get('/connection')
+  async getConnection(): Promise<string> {
+    this.userRepository.save();
+    this.mailService.send();
+    this.emailService.send();
+
+    console.info(this.memberService.getConnectionName());
+    this.memberService.sendEmail();
+
+    return this.connection.getName();
+  }
+
+  @Get('/hello')
+  async sayHello(@Query('name') name: string): Promise<string> {
+    return this.service.sayHello(name);
+  }
+
+  @Get('/view/hello')
+  viewHello(@Query('name') name: string, @Res() response: Response) {
+    response.render('index.html', {
+      title: 'Template Engine',
+      name: name,
+    });
+  }
+
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('Success Set Cookie');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name'] as string;
+  }
+
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return { data: 'Hello JSON' };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return { url: '/api/users/sample-response', statusCode: 301 };
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'Hello NestJS';
+  }
+}
